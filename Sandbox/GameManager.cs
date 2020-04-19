3681e4f35cad0ee2a8c8dcf4072c08ea6896922c
Zhang-Dev-byte <63158613+Zhang-Dev-byte@users.noreply.github.com>
2020-04-19 10:49:10 -0700

    Updated Base

--- Sandbox/GameManager.cs
+++ Sandbox/GameManager.cs
@@ -15,14 +15,7 @@ namespace Sandbox
         public const int height = 800;
 
         //OBJECTS
-        private Sprite rect = Graphics.Draw("player.png", new Vector2f(0, 0));
-        private Sprite rect2 = Graphics.Draw("player.png", new Vector2f(20, 300));
-        private Sprite background = Graphics.Draw("background.png", new Vector2f(0, 0));
-        private Vector2f pos;
-        private Vector2f mousePos;
         private static Font _font = Graphics.LoadFont("default.ttf");
-
-        private Button button = Graphics.DrawButton(_font, "Hello", Graphics.DrawRect(new Vector2f(100, 50), Color.Black, 5, Color.Black, new Vector2f(20, 0)));
         
         //LISTS
         private List<Button> buttons = new List<Button>();
@@ -36,9 +29,6 @@ namespace Sandbox
             window.MouseButtonPressed += HandleMouseButtonInput;
             window.MouseMoved += HandleMouseInput;
             
-            Collision.AddCollisionTriggerSprite(rect,rect2);
-            Physics.ImplementPhysicsSprite(rect);
-            
             Renderer.Render(window,objects);
             UIRenderer.RenderButtons(window,buttons);
         }
@@ -46,12 +36,8 @@ namespace Sandbox
         public void Start()
         {
             //OBJECTS
-            objects.Add(background);
-            objects.Add(rect);
-            objects.Add(rect2);
             
             //BUTTONS
-            buttons.Add(button);
             
             //FRAMES
             
@@ -65,24 +51,16 @@ namespace Sandbox
         void HandleInput(object sender, KeyEventArgs e)
         {
             
-            pos = rect.Position;
-            pos.X += Input.GetAxisX(e) * 0.0005f;
-            pos.Y += Input.GetAxisY(e) * 0.0005f;
-            rect.Position = pos;
         }
 
         void HandleMouseButtonInput(object sender, MouseButtonEventArgs e)
         {
-            if (Button.GetClick(e,mousePos,button))
-            {
-                Console.WriteLine("Clicked");
-            }
             
         }
 
         void HandleMouseInput(object sender,MouseMoveEventArgs e)
         {
-            mousePos = new Vector2f(e.X,e.Y);
+			
         }
     }
 }
