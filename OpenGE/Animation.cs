67d4c1dd5aee5b3114713ecada5988ece6f1e84c
Zhang-Dev-byte <63158613+Zhang-Dev-byte@users.noreply.github.com>
2020-04-20 16:21:07 -0700

    Animation System

---
+++ OpenGE/Animation.cs
@@ -0,0 +1,48 @@
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace OpenGE
+{
+    public class Animation
+    {
+        public int frameWidth;
+        public int frameHeight;
+        public float Delay;
+        public int frames;
+        private static Texture sheet;
+        private static IntRect rectSource = new IntRect(0, 0, 16, 16);
+        public Sprite sprite;
+        private Clock clock;
+        public Animation(Texture _sheet,int _frameWidth,int _frameHeight,float _Delay,int _frames)
+        {
+            frameWidth = _frameWidth;
+            frameHeight = _frameHeight;
+            sheet = _sheet;
+            Delay = _Delay;
+            frames = _frames;
+            sprite = new Sprite(sheet, rectSource);
+            clock = new Clock();
+            rectSource = new IntRect(0, 0, frameWidth, frameHeight);
+            
+        }
+
+        public void Update()
+        {
+            if (clock.ElapsedTime.AsSeconds() > Delay)
+            {
+                if (rectSource.Left == frameWidth * frames)
+                {
+                    rectSource.Left = 0;
+                    clock.Restart();
+                }
+                else
+                {
+                    rectSource.Left += frameWidth;
+                    clock.Restart();
+                }
+
+                sprite.TextureRect = rectSource;
+            }
+        }
+    }
+}
