4bc85ded33a92609b28d1fa6ae94c0d2aa7bf86c
Zhang-Dev-byte <63158613+Zhang-Dev-byte@users.noreply.github.com>
2020-04-20 20:47:08 -0700

    Text Rendering

---
+++ OpenGE/TextRenderer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using SFML.Graphics;
+
+namespace OpenGE
+{
+    public class TextRenderer
+    {
+        public static void Render(List<Text> text,RenderWindow window)
+        {
+            foreach (Text t in text)
+            {
+                window.Draw(t);
+            }
+        }
+    }
+}
