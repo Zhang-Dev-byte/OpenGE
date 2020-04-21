be6f572086f097a105ec072880229a444e2e7b09
Zhang-Dev-byte <63158613+Zhang-Dev-byte@users.noreply.github.com>
2020-04-20 20:43:29 -0700

    Logging

---
+++ OpenGE/Debug.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OpenGE
+{
+    public class Debug
+    {
+        public static void Log(object message)
+        {
+            Console.WriteLine(message.ToString());
+        }
+        
+    }
+}
