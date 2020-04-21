5a725a1690ce0ef9ef1816b747a7a5109896a3de
Zhang-Dev-byte <63158613+Zhang-Dev-byte@users.noreply.github.com>
2020-04-20 17:16:48 -0700

    Renderer

---
+++ OpenGE/AnimationRenderer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using SFML.Graphics;
+
+namespace OpenGE
+{
+    public class AnimationRenderer
+    {
+        public static void Render(List<Animation> animations, RenderWindow window)
+        {
+            foreach (Animation anim in animations)
+            {
+                window.Draw(anim.sprite);
+            }
+        }
+    }
+}
