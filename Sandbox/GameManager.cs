893732749c4d65b3e237178bd8ee4ad6a36388e7
Zhang-Dev-byte <63158613+Zhang-Dev-byte@users.noreply.github.com>
2020-04-29 15:47:12 -0700

    Create GameManager.cs

---
+++ Sandbox/GameManager.cs
@@ -0,0 +1,76 @@
+
+using OpenGE;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace Sandbox
+{
+    public class GameManager : Manager
+    {
+        //CONSTANTS
+        public static string game = "Hello";
+        public static int width = 800;
+        public static int height = 800;
+
+        //OBJECTS
+        private static Font _font = Graphics.LoadFont("default.ttf");
+        
+        //LISTS
+        private List<Button> buttons = new List<Button>();
+        private List<Drawable> objects = new List<Drawable>();
+        private List<Frame> frames = new List<Frame>();
+        private List<Animation> animations = new List<Animation>();
+		private List<Text> text= new List<Text>();
+		
+		//AUDIO
+        public void Render(RenderWindow window)
+        {
+            //HANDLE EVENTS
+            window.KeyPressed += HandleInput;
+            window.MouseButtonPressed += HandleMouseButtonInput;
+            window.MouseMoved += HandleMouseInput;
+            
+            Renderer.Render(window,objects);
+            UIRenderer.RenderButtons(window,buttons);
+			AnimationRenderer.Render(animations,window);
+			TextRenderer.Render(text,window);
+        }
+
+        public void Start()
+        {
+			//PLAY AUDIO
+			
+            //OBJECTS
+            
+            //BUTTONS
+            
+            //FRAMES
+			
+			//ANIMATIONS
+			
+			//TEXT
+            
+            //Run all the GEObject "Start" Methods
+        }
+
+        public void Update()
+        {
+            //Run all the GEObject "Update" Methods
+        }
+        void HandleInput(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        void HandleMouseButtonInput(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        void HandleMouseInput(object sender,MouseMoveEventArgs e)
+        {
+			
+        }
+    }
+}
