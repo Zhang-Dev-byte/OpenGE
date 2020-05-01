8fc857112b779fec4b5aa6e722879266b91e5eed
Zhang-Dev-byte <63158613+Zhang-Dev-byte@users.noreply.github.com>
2020-05-01 08:56:05 -0700

    Update GameCore.cs

--- Sandbox/GameCore.cs
+++ Sandbox/GameCore.cs
@@ -1,14 +1,24 @@
-﻿using System;
+using System;
+using System.Runtime.InteropServices;
 
 namespace Sandbox
 {
     class GameCore
     {
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(int Handle, int showState);
+
+        [DllImport("kernel32.dll")]
+        public static extern int GetConsoleWindow();
+
         /// <summary>
         /// The main entry point of the application
         /// </summary>
         static void Main(string[] args)
         {
+            int window = GetConsoleWindow();
+            ShowWindow(window, (int)GameManager.state);
+
             OpenGE.Renderer.Window.Create(GameManager.width,GameManager.height,GameManager.game,new GameManager());
         }
     }
