0922105fb696fc6fe8011b85bde4decc94511fea
Zhang-Dev-byte <63158613+Zhang-Dev-byte@users.noreply.github.com>
2020-05-01 08:56:21 -0700

    Add files via upload

---
+++ OpenGE/ShowState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenGE
+{
+    namespace Utilities
+    {
+        public enum ShowState
+        {
+            Debug = 1,
+            Release = 0
+        }
+    }
+}
