482673ca7178b24d0b9146cb8eb8e84f04c2afe4
Zhang-Dev-byte <63158613+Zhang-Dev-byte@users.noreply.github.com>
2020-05-03 19:57:35 -0700

    Network fixes

--- OpenGE/Client.cs
+++ OpenGE/Client.cs
@@ -29,7 +29,23 @@ namespace OpenGE
                 try
                 {
                     Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
                     socket.Connect(new IPEndPoint(IPAddress.Parse(ipAddress), Port));
+
+                    TcpClient client = new TcpClient(ipAddress, Port);
+                    Byte[] data = System.Text.Encoding.ASCII.GetBytes("Connection Request");
+                    NetworkStream stream = client.GetStream();
+                    stream.Write(data, 0, data.Length);
+
+                    Console.WriteLine("Sent: {0}", "Connection Request");
+                    data = new Byte[256];
+                    String responseData = String.Empty;
+                    Int32 bytes = stream.Read(data, 0, data.Length);
+                    responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                    Console.WriteLine("Received: {0}", responseData);
+                    stream.Close();
+                    client.Close();
+
                     Debug.Log($"Connected to {ipAddress} on port {Port}...");
                 }
                 catch
